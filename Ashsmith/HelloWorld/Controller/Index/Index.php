8b07bda95564111962119a3bd2201af186cbee0d
Ash Smith <ash.smith@meanbee.com>
2015-08-15 15:33:38 +0100

    General improvements.
    - Fixed the controller to not use deprecated code.
    - Template file will spit out the class name to show how it works.
    - Added a composer file to make this bad boy installable via composer.

--- Ashsmith/HelloWorld/Controller/Index/Index.php
+++ Ashsmith/HelloWorld/Controller/Index/Index.php
@@ -3,6 +3,23 @@ namespace Ashsmith\HelloWorld\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory resultPageFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    )
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
 
     /**
      * Default customer account page
@@ -11,8 +28,6 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        $this->_view->loadLayout();
-        $this->_view->getLayout()->initMessages();
-        $this->_view->renderLayout();
+        $this->resultPageFactory->create();
     }
 }
